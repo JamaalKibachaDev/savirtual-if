f14c5da27c58f69bda09532cd0ccb883b7fbd50e
Jamaal Kanyau <kibachajamaal@gmail.com>
2025-06-28 00:05:28 +0300

    Bugs

--- src/components/PilotTestimony.js
+++ src/components/PilotTestimony.js
@@ -1,35 +1,32 @@
-import React from 'react'
+import React from "react";
 import { FaQuoteLeft } from "react-icons/fa";
 import { FaQuoteRight } from "react-icons/fa";
 
+import { PilotTestimonies } from "../assets/data";
 
-import { PilotTestimonies } from '../assets/data';
-
-import './css/Header.css';
+import "./css/Header.css";
 
 const PilotTestimony = () => {
   return (
     <>
-    <div className='container p-5'>
-        <div className='left d-flex justify-content-center'>
-            <FaQuoteLeft className='quote__left'/>
+      <div className="container p-5">
+        <div className="left d-flex justify-content-center">
+          <FaQuoteLeft className="quote__left" />
         </div>
-        <div className='text text-center'>
-          {PilotTestimonies.map(({testimonial}) => (
-            <p>
-              {testimonial}
-            </p>
+        <div className="text text-center">
+          {PilotTestimonies.map(({ testimonial }) => (
+            <p>{testimonial}</p>
           ))}
-          <blockquote className='text-muted fst-italic'>
+          <blockquote className="text-muted fst-italic">
             - SAAV Senior Pilot
           </blockquote>
         </div>
-        <div className='right d-flex justify-content-center'>
-            <FaQuoteRight className='quote__right'/>
+        <div className="right d-flex justify-content-center">
+          <FaQuoteRight className="quote__right" />
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PilotTestimony
+export default PilotTestimony;
